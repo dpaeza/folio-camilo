23916187f6a5621deb768e724aae82a6a40f17f3
dpaeza10 <daniela.paez@inchcape.com>
2025-06-06 20:20:26 -0500

    Update cursor styles to enhance z-index for improved visibility

--- components/common/cursor.tsx
+++ components/common/cursor.tsx
@@ -10,8 +10,8 @@ import { gsap, Linear } from "gsap";
 import { IDesktop, isSmallScreen } from "pages";
 
 const CURSOR_STYLES = {
-  CURSOR: "fixed hidden bg-white w-4 h-4 select-none pointer-events-none z-50",
-  FOLLOWER: "fixed hidden h-8 w-8 select-none pointer-events-none z-50",
+  CURSOR: "fixed hidden bg-white w-4 h-4 select-none pointer-events-none z-[9999]",
+  FOLLOWER: "fixed hidden h-8 w-8 select-none pointer-events-none z-[9999]",
 };
 
 const Cursor = ({ isDesktop }: IDesktop) => {
